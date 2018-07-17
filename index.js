2a14086ec4d17f304c30a3a125060690a35325c1
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-17 11:06:55 +0200

    Fixed preview

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 
 const startWith = (...xs) => inputSource => (start, outputSink) => {
   if (start !== 0) return;
+  xs = xs.map(v => [1, v]);
   let inputTalkback;
+  let inited = false;
+  let unsubed = false;
   inputSource(0, (it,id) => {
     if (it === 0){
       inputTalkback = id;
-      outputSink(0, ot => {
-        if (ot === 2) inputTalkback(ot);
+
+      outputSink(0, (ot, od) => {
+        if (ot === 2) {
+          unsubed = true;
+        }
+        inputTalkback(ot, od);
       });
-      xs.forEach(x => outputSink(1, x));
-    } else {
-      outputSink(it, id);
+
+      while (xs.length !== 0 && !unsubed) {
+        outputSink(...xs.shift());
+      }
+      inited = true;
+      return;
     }
-    if (it !== 2) inputTalkback(1);
+
+    if (!inited) {
+      xs.push([it, id]);
+      return;
+    }
+
+    outputSink(it, id);
   });
 };
 
